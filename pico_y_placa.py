5c14fccf61cacdf4baa10dfed7d9fb6276a07091
Mateo Pozo <mateopozoruiz02@hotmail.com>
2024-09-15 20:24:22 -0500

    Update set_license_plate method
    Included a validation mechanism with regular expressions

--- pico_y_placa.py
+++ pico_y_placa.py
@@ -1,8 +1,14 @@
+import re
+
 class Car:
     def __init__(self, license_plate):
         self._license_plate = license_plate
 
     def set_license_plate(self, license_plate):
+        # Validate license plate format to be like 'PBX-1234'
+        if not re.match(r'^[A-Z]{3}-\d{4}$', license_plate):
+            raise ValueError('Invalid license plate format. Must be like "PBX-1234"')
+        
         self._license_plate = license_plate
 
     def get_license_plate(self):
