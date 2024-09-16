d80ad5564002752970aaf6c21b72c049167c3e54
Mateo Pozo <mateopozoruiz02@hotmail.com>
2024-09-15 21:47:33 -0500

    Add PicoYPlaca Class and its constructor
    The datetime module was included to manage dates in the class

--- pico_y_placa.py
+++ pico_y_placa.py
@@ -1,4 +1,5 @@
 import re
+import datetime
 
 class Car:
     def __init__(self, license_plate):
@@ -19,3 +20,20 @@ class Car:
     
     def __str__(self):
         return f'Car with license plate {self._license_plate}'
+    
+class PicoYPlaca: 
+    def __init__(self):
+        # Define the restriction days and plate numbers
+        self.restriction_schedule = {
+            0: [1, 2], # Monday
+            1: [3, 4], # Tuesday
+            2: [5, 6], # Wednesday 
+            3: [7, 8], # Thursday
+            4: [9, 0] # Friday
+        }
+
+        # Define the restriction hours
+        self.restriction_hours = [
+            (datetime.strptime('06:00', '%H:%M'), datetime.strptime('09:00', '%H:%M')),
+            (datetime.strptime('16:00', '%H:%M'), datetime.strptime('20:00', '%H:%M'))
+        ]
