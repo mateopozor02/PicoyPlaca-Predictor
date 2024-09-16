4d6295c7447ec36bda1bfd4e9137c058a9189b27
Mateo Pozo <mateopozoruiz02@hotmail.com>
2024-09-15 20:26:06 -0500

    Update the Car class constructor
    Included the validation mechanism from the setter method

--- pico_y_placa.py
+++ pico_y_placa.py
@@ -2,7 +2,7 @@ import re
 
 class Car:
     def __init__(self, license_plate):
-        self._license_plate = license_plate
+        self._license_plate = self.set_license_plate(license_plate)
 
     def set_license_plate(self, license_plate):
         # Validate license plate format to be like 'PBX-1234'
